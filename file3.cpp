a5834c8dd9ea576c7f3b0c6040fec45e8929835e
Abhishek Kumar <97536635+abhishek-kumar1012@users.noreply.github.com>
2025-01-12 13:32:44 +0530

    Update file3.cpp

--- file3.cpp
+++ file3.cpp
@@ -1,1 +1,59 @@
 
+You are given a string 'str' and an integer ‘K’. Your task is to find the length of the largest substring with at most ‘K’ distinct characters.
+
+For example:
+You are given ‘str’ = ‘abbbbbbc’ and ‘K’ = 2, then the substrings that can be formed are [‘abbbbbb’, ‘bbbbbbc’]. Hence the answer is 7.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= T <= 10
+1 <= K <= 26
+1 <= |str| <= 10^6
+
+The string str will contain only lowercase alphabets.    
+
+Time Limit: 1 sec
+Note:
+You do not need to print anything. It has already been taken care of. Just implement the function.
+Sample Input 1:
+2
+2
+abbbbbbc
+3
+abcddefg
+Sample Output 1:
+7
+4
+Explanation:
+For the first test case, ‘str’ = ‘abbbbbbc’ and ‘K’ = 2, then the substrings that can be formed are [‘abbbbbb’, ‘bbbbbbc’]. Hence the answer is 7.
+
+For the second test case, ‘str’ = ‘abcddefg’ and ‘K’ = 3, then the substrings that can be formed is [‘cdde’, ‘ddef’]. Hence the answer is 4.
+Sample Input 2:
+2
+3
+aaaaaaaa
+1
+abcefg
+Sample Output 2:
+8   
+1   
+  code:  
+int kDistinctChars(int k, string &str)
+{
+
+    int n=str.size();
+    int l=0;
+    int r=0;
+    int len=0;
+     unordered_map<char,int>mp;
+    while(r<n){
+       mp[str[r]]++;
+       while(mp.size()>k){
+           mp[str[l]]--;
+           if(mp[str[l]]==0)mp.erase(str[l]);
+           l++;
+       }
+           len = max(len, r - l + 1);
+           r++;    
+    }
+    return len;
+}
